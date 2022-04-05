a25f326daee6b033a37f0b8ad20ca3113647c35f
Marcus-K-Thorsen <marc303i@stud.kea.dk>
2022-04-05 15:22:08 +0200

    Tilføjede abstract metode showMood der tager en boolean og er implementeret i Dog og Cat class

--- src/Cat.java
+++ src/Cat.java
@@ -1,6 +1,15 @@
 public class Cat extends Animal{
 
 
+
+  public void showMood(boolean isHappy) {
+    if (isHappy) {
+      System.out.println("spinder");
+    } else {
+      System.out.println("hvæsser");
+    }
+  }
+
   public boolean eat(String foodType) {
     return true;
   }
--- src/Dog.java
+++ src/Dog.java
@@ -2,6 +2,14 @@ public class Dog extends Animal{
 
 
 
+  public void showMood(boolean isHappy) {
+    if (isHappy) {
+      System.out.println("logrer");
+    } else {
+      System.out.println("knurrer");
+    }
+  }
+
   public boolean eat(String foodType) {
     return true;
   }
